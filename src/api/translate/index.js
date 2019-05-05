e9b1b8db3fc9f5b935927d92d07d5e9db783cd66
Frans Huang <franssiswanto@gmail.com>
2019-05-05 18:20:06 +0800

    feat: shortTime for making a short time

--- src/api/translate/index.js
+++ src/api/translate/index.js
@@ -9,4 +9,16 @@ export default class TranslateAPI{
     words = words.replace("day", "hari");
     return words;
   }
+
+  static shortTime(words){
+    words = words.replace("hours", "j");
+    words = words.replace("hour", "j");
+    words = words.replace("ago", "");
+    words = words.replace("minutes", "m");
+    words = words.replace("minute", "m");
+    words = words.replace("days", "h");
+    words = words.replace("day", "h");
+    words = words.replace(/ /g,'');
+    return words;
+  }
 }
