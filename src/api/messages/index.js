00e287ae39fe8833475a95460cdf41475a33c334
Frans Huang <franssiswanto@gmail.com>
2019-06-05 17:33:31 +0800

    feat: pagination for chat
    This is related to issue #46 and PR #51

--- src/api/messages/index.js
+++ src/api/messages/index.js
@@ -1,10 +1,54 @@
 import firebase from "react-native-firebase";
 
-import PeopleAPI from "src/api/people";
 import { RoomsCollection, MessagesCollection } from "src/api/database/collection";
 import { Document } from "src/api/database/document";
 
 export default class MessagesAPI{
+
+  constructor(roomId=null){
+    this.roomId = roomId;
+    this.lastDocumentSnapshot = null;
+    this.messagePaginationLimit = 25;
+  }
+
+  /**
+   * 
+   * @param {String} roomId 
+   */
+  getMessageReference(roomId){
+    const roomsCollection = new RoomsCollection();
+    const messagesCollection = new MessagesCollection();
+    const roomDocument = new Document(roomId);
+
+    const db = firebase.firestore();
+    const roomsRef = db.collection(roomsCollection.getName()).doc(roomDocument.getId());
+    const messagesRef = roomsRef.collection(messagesCollection.getName())
+    return messagesRef;
+  }
+
+  getNext(){
+    if(this.lastDocumentSnapshot && this.roomId){
+      const messagesRef = this.getMessageReference(this.roomId);
+      const queryRef = messagesRef.orderBy("sentTime", "desc").startAfter(this.lastDocumentSnapshot);
+      return queryRef.limit(this.messagePaginationLimit).get().then(querySnapshot => {
+        const { messages, lastDocumentSnapshot } = this.parseQuerySnapshotToMessages(querySnapshot);
+        this.lastDocumentSnapshot = lastDocumentSnapshot;
+        return messages
+      })
+    }else return Promise.resolve([]);
+  }
+
+  parseQuerySnapshotToMessages(querySnapshot){
+    let lastDocumentSnapshot = null;
+    const messages = [];
+    querySnapshot.forEach(documentSnapshot => {
+      lastDocumentSnapshot = documentSnapshot;
+      const payload = Object.assign({ isSent: !documentSnapshot.metadata.hasPendingWrites }, documentSnapshot.data());
+      messages.push(payload);
+    })
+    return { messages, lastDocumentSnapshot }
+  }
+
   /**
    * This function will trigger for ofline storage as well
    * 
@@ -12,21 +56,12 @@ export default class MessagesAPI{
    * @param {function} callback - a callback for this real-time function. It requires one parameter `messages`
    */
   getMessagesWithRealTimeUpdate(roomId, callback){
-    const roomsCollection = new RoomsCollection();
-    const messagesCollection = new MessagesCollection();
-    const roomDocument = new Document(roomId);
-    
-    const db = firebase.firestore ();
-    const roomsRef = db.collection(roomsCollection.getName()).doc(roomDocument.getId());
-    const messagesRef = roomsRef.collection(messagesCollection.getName())
-    const filteredMessagesRef = messagesRef.orderBy("sentTime", "desc").limit(25);
+    const messagesRef = this.getMessageReference(roomId);
+    const filteredMessagesRef = messagesRef.orderBy("sentTime", "desc").limit(this.messagePaginationLimit);
     filteredMessagesRef.onSnapshot({ includeMetadataChanges: true }, snapshot => {
-      const messages = [];
-      snapshot.forEach(documentSnapshot => {
-        const payload = Object.assign({ isSent: !documentSnapshot.metadata.hasPendingWrites }, documentSnapshot.data());
-        messages.push(payload);
-      })
-      callback(messages);
+      const { messages, lastDocumentSnapshot } = this.parseQuerySnapshotToMessages(snapshot);
+      this.lastDocumentSnapshot = lastDocumentSnapshot;
+      callback(messages, lastDocumentSnapshot);
     })
   }
 
