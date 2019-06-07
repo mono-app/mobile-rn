e352cf0a9bd2b1f518575254a206b477c20b843a
Frans Huang <franssiswanto@gmail.com>
2019-06-07 23:41:25 +0800

    fix: photo grid is using FastImage

--- modules/Moments/components/PhotoGrid/index.js
+++ modules/Moments/components/PhotoGrid/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import RNFS from "react-native-fs";
-import { View, Image, Dimensions, StyleSheet, TouchableOpacity } from "react-native";
+import FastImage from "react-native-fast-image";
+import { View, Dimensions, StyleSheet, TouchableOpacity } from "react-native";
 import { Text } from "react-native-paper";
 
 export default class PhotoGrid extends React.Component{
@@ -33,15 +34,25 @@ export default class PhotoGrid extends React.Component{
     return(
       <TouchableOpacity onPress={this.handleContainerPress} style={{ backgroundColor: "white", flex: 1 }}> 
         {this.props.images.length !== 0?(
-          <Image source={{ uri: firstImage.image.uri, cache: "force-cache" }} style={{height: 200, ...styles.singleImage}}/>
+          <FastImage 
+            source={{ uri: firstImage.image.uri }} 
+            resizeMode="cover"
+            style={{ height: 200, ...styles.singleImage }}/>
         ):<View/>}
 
         <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-evenly" }}>
           {this.props.images.map((item, index) => {
-            if((index > 0 && index < 4) || index === 4 && remainingImageCount === 0) return <Image key={index} source={{ uri: item.image.uri, cache: "force-cache" }} style={{ height: (window.width/4), flex: 1, ...styles.singleImage}}/>
-            else if(index === 4 && remainingImageCount > 0) return (
+            if((index > 0 && index < 4) || index === 4 && remainingImageCount === 0) {
+              return (
+                <FastImage 
+                  key={index} 
+                  resizeMode="cover"
+                  source={{ uri: item.image.uri,  }} 
+                  style={{ height: (window.width/4), flex: 1, alignItems: "stretch" }}/>
+              )
+            }else if(index === 4 && remainingImageCount > 0) return (
               <View key={index} style={{ alignSelf: "stretch", flex: 1, height: (window.width/4) }}>
-                <Image source={{ uri: item.image.uri, cache: "force-cache" }} style={{ alignSelf: "stretch", flex: 1, resizeMode: "cover" }}/>
+                <FastImage source={{ uri: item.image.uri }} style={{ alignSelf: "stretch", flex: 1 }} resizeMode="cover"/>
                 <View style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0, backgroundColor: "rgba(0, 0, 0, .7)", alignItems: "center", justifyContent: "center" }}>
                   <Text style={{ color: "white" }}>+ {remainingImageCount}</Text>
                 </View>
@@ -57,5 +68,5 @@ export default class PhotoGrid extends React.Component{
 PhotoGrid.defaultProps = { images: [] }
 
 const styles = StyleSheet.create({
-  singleImage: { alignItems: "stretch", resizeMode: "cover" }
+  singleImage: { alignItems: "stretch" }
 })
