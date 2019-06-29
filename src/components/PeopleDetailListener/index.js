156c0804a08e7735c5553bd12799c27ddddaa557
Frans Huang <franssiswanto@gmail.com>
2019-06-29 17:17:49 +0800

    fix: caching

---
+++ src/components/PeopleDetailListener/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import firebase from "react-native-firebase";
+import SInfo from "react-native-sensitive-info";
+
+import { UserCollection } from "src/api/database/collection";
+import { Document } from "src/api/database/document";
+
+export default class PeopleDetailListener extends React.PureComponent{
+  loadData = async () => {
+    const { peopleEmail } = this.props;
+    if(peopleEmail && this.listener === null){
+      // Get data from cache
+      const people = await SInfo.getItem(`people!${peopleEmail}`, {});
+      if(people && this.props.onChange) {
+        const peopleData = JSON.parse(people);
+        this.props.onChange(peopleData);
+      }
+      
+      // get new data from server, and store it in cache
+      const db = firebase.firestore();
+      const usersCollection = new UserCollection();
+      const userDocument = new Document(peopleEmail);
+      const userRef = db.collection(usersCollection.getName()).doc(userDocument.getId());
+      this.listener = userRef.onSnapshot({ includeMetadataChanges: false }, async (documentSnapshot) => {
+        const payload = { id: documentSnapshot.id, ...documentSnapshot.data() };
+        const { applicationInformation } = payload;
+        const profilePicture = applicationInformation.profilePicture? applicationInformation.profilePicture.downloadUrl: "https://picsum.photos/200/200/?random";
+        payload.applicationInformation.profilePicture = profilePicture;
+
+        const jsonPayload = JSON.stringify(payload);
+        await SInfo.setItem(`people!${peopleEmail}`, jsonPayload, {});
+        if(this.props.onChange) this.props.onChange(payload);
+      })
+    }
+  }
+  
+  constructor(props){
+    super(props);
+
+    this.listener = null;
+    this.loadData = this.loadData.bind(this);
+  }
+
+  componentDidMount(){ this.loadData(); }
+  componentDidUpdate(){ this.loadData(); }
+  componentWillUnmount(){ if(this.listener) this.listener(); }
+
+  render(){ return null; }
+}
+
+PeopleDetailListener.defaultProps = { peopleEmail: null, onChange: null }
