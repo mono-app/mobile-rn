4f4805a3fb34830860c66b358553a7f1126f0898
Dennis <dennis.tgt@gmail.com>
2019-08-07 06:49:12 +0700

    add new library - async storage - RNDownloader

--- android/app/src/main/java/com/zannete/monoapp/MainApplication.java
+++ android/app/src/main/java/com/zannete/monoapp/MainApplication.java
@@ -2,6 +2,8 @@ package com.zannete.monoapp;
 
 import android.app.Application;
 import com.facebook.react.ReactApplication;
+import com.eko.RNBackgroundDownloaderPackage;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.react.rnspinkit.RNSpinkitPackage;
 import com.dylanvann.fastimage.FastImageViewPackage;
 
@@ -41,6 +43,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNBackgroundDownloaderPackage(),
+            new AsyncStoragePackage(),
           new RNSpinkitPackage(),
           new FastImageViewPackage(),
           new RNFSPackage(),
