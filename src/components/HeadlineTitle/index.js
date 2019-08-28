b22550f931426c2ca97164d0a0e3916a20eda523
Frans Huang <franssiswanto@gmail.com>
2019-08-28 17:45:09 +0800

    change UI for app

--- src/components/HeadlineTitle/index.js
+++ src/components/HeadlineTitle/index.js
@@ -1,11 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 import { StyleSheet } from "react-native";
 import { Headline } from "react-native-paper";
 
-export default function HeadlineTitle(props){
+function HeadlineTitle(props){
   const styles = StyleSheet.create({
     default: { fontSize: 36, lineHeight: 48, fontWeight: "900" }
   });
 
   return <Headline style={[ styles.default, props.style ]}>{props.children}</Headline>
 }
+HeadlineTitle.propTypes = { children: PropTypes.string.isRequired }
+export default HeadlineTitle;
