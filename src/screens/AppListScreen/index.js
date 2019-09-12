47fd93cd54c5b2280aebb94003d5496682582e27
Frans Huang <franssiswanto@gmail.com>
2019-09-12 17:10:38 +0800

    add componentDidMount replacement

--- src/screens/AppListScreen/index.js
+++ src/screens/AppListScreen/index.js
@@ -8,21 +8,36 @@ import { default as MaterialCommunityIcons } from "react-native-vector-icons/Mat
 import MenuListItemWithIcon from "src/components/MenuListItemWithIcon";
 import HeadlineTitle from "src/components/HeadlineTitle";
 
+import { withCurrentUser } from "src/api/people/CurrentUser";
+import SchoolAPI from "modules/Classroom/api/school";
+
 function AppListScreen(props){  
+  const [ data, setData ] = React.useState([]);
+
   const handleItemPress = (item) => {
     const { navigateTo } = item;
     props.navigation.navigate(navigateTo);
   }
+
+  const fetchData = async () => {
+    const schoolList = await SchoolAPI.getUserSchools(this.props.currentUser.email);
+    if(schoolList.length>0){
+      data.push({ title: "Classroom", icon: <MaterialIcons name="class" size={24}/>, navigateTo: "Classroom" })
+    }
+    data.push({ title: "News", icon: <MaterialCommunityIcons name="newspaper" size={24}/>, navigateTo: "News" })
+    setData(data);
+  }
+
+  React.useEffect(() => {
+    fetchData();
+  }, [])
 
   return(
     <View style={{ flex: 1 }}>
       <AppHeader style={{ backgroundColor: "transparent" }}/>
       <HeadlineTitle style={{ marginLeft: 16, marginRight: 16, marginTop: 8 }}>Applikasi</HeadlineTitle>
       <FlatList
-        data={[
-          { title: "Classroom", icon: <MaterialIcons name="class" size={24}/>, navigateTo: "Classroom" },
-          { title: "News", icon: <MaterialCommunityIcons name="newspaper" size={24}/>, navigateTo: "News" }
-        ]}
+        data={data}
         renderItem={({ item, index }) => {
           return <MenuListItemWithIcon key={index} item={item} onPress={handleItemPress}/>
         }}/>
@@ -30,4 +45,4 @@ function AppListScreen(props){
   )
 }
 AppListScreen.navigationOptions = { header: null }
-export default AppListScreen;
+export default withCurrentUser(AppListScreen);
