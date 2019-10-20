cbbdab1dd4e3caffa586b5f25969a10ed38ed616
Frans Huang <franssiswanto@gmail.com>
2019-10-20 12:05:19 +0800

    Refactor code dan ganti header

--- modules/Classroom/components/Header/index.js
+++ modules/Classroom/components/Header/index.js
@@ -1,26 +1,9 @@
 import React from "react";
-import Navigator from "src/api/navigator";
-import { StackActions } from "react-navigation";
-import { StyleSheet, View } from "react-native";
 
-import { Appbar, Subheading, Caption } from "react-native-paper";
+import AppHeader from "src/components/AppHeader";
 
 function Header(props){ 
-  const styles = StyleSheet.create({ default: { backgroundColor: "white"} })
-  handleBackPress = () => {
-    props.navigation.goBack(null)
-    // const navigator = new Navigator(props.navigation);
-    // navigator.resetTo("Home", StackActions, {key: "AppTab"});
-  }
-
-  return(
-    <Appbar.Header style={[ styles.default, props.styles ]}>
-      <Appbar.BackAction onPress={this.handleBackPress}/>
-      <View style={{ display: "flex", flexDirection: "row", alignItems: "center", flexGrow: 1 }}>
-        <Subheading>{props.title}</Subheading>
-        <Caption>{props.subtitle}</Caption>
-      </View>
-    </Appbar.Header>
-  )
+  handleBackPress = () => props.navigation.goBack(null)
+  return <AppHeader {...props} overrideBack={handleBackPress}/>
 }
 export default Header;
