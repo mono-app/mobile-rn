ac78198cee272ce5f365cf2c3aeaddf1e1844bbc
Frans Huang <franssiswanto@gmail.com>
2019-10-20 14:02:22 +0800

    fix: better flow for permissions

--- src/api/storage/index.js
+++ src/api/storage/index.js
@@ -1,8 +1,47 @@
+import IOSImagePicker from "react-native-image-crop-picker";
+import DocumentPicker from 'react-native-document-picker';
+import Permissions from "react-native-permissions";
 import firebase from "react-native-firebase";
-import uuid from "uuid/v4"
 import RNFS from "react-native-fs"
+import uuid from "uuid/v4"
+import { Platform } from "react-native";
 
 export default class StorageAPI{
+
+  static async requestGalleryPermission(){
+    const permissionString = (Platform.OS === "ios")? "photo": "storage";
+    const permissionResponse = await Permissions.request(permissionString);
+    return Promise.resolve(permissionResponse);
+  }
+
+  static async checkGalleryPermission(){
+    const permissionString = (Platform.OS === "ios")? "photo": "storage";
+    const permissionResponse = await Permissions.check(permissionString);
+    return Promise.resolve(permissionResponse);
+  }
+
+  static async openGallery(multiple=true){
+    const galleryPermission = await StorageAPI.checkGalleryPermission();
+    if(galleryPermission === "denied") return Promise.reject(galleryPermission);
+    else if(galleryPermission === "undetermined") {
+      await StorageAPI.requestGalleryPermission();
+      StorageAPI.openGallery(multiple);
+    }
+
+    if(Platform.OS === "ios"){
+      const results = await IOSImagePicker.openPicker({ mediaType: "photo", multiple });
+      const normalizedResults = results.map((image) => {
+        return { uri: image.sourceURL, size: image.size }
+      })
+      return Promise.resolve(normalizedResults);
+    }else if(Platform.OS === "android" && multiple){
+      const results = await DocumentPicker.pickMultiple({ type: [DocumentPicker.types.images] });
+      return Promise.resolve(results);
+    }else if(Platform.OS === "android" && !multiple){
+      const results = await DocumentPicker.pick({ type: [DocumentPicker.types.images] });
+      return Promise.resolve([results]);
+    }
+  }
 
   /**
    * 
