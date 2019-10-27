221f7513a9466e3753cbb23389823032901cbec1
Frans Huang <franssiswanto@gmail.com>
2019-10-27 16:25:13 +0800

    database api for online and offline

--- src/api/database/index.js
+++ src/api/database/index.js
@@ -1,6 +1,15 @@
 import firebase from "react-native-firebase";
+import OfflineDatabase from "src/api/database/offline";
 
-export class Database{
-  constructor(){ this.db = firebase.firestore(); }
-  getDatabase(){ return this.db; }
+export default class Database{
+  static chooseDatabase(online){
+    if(online) return firebase.firestore();
+    else return OfflineDatabase.database;
+  }
+
+  static async listen(func, online=true){ return await func(Database.chooseDatabase(online)) }
+  static async insert(func, online=true){ return await func(Database.chooseDatabase(online)) }
+  static async update(func, online=true){ return await func(Database.chooseDatabase(online)) }
+  static async delete(func, online=true){ return await func(Database.chooseDatabase(online)) }
+  static async get(func, online=false){ return await func(Database.chooseDatabase(online)) }
 }
